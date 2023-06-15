419a527639bc0e0ed9eecf9a831c56c308268678
Gagi Adamashvili <gagiadamashvili@gmail.com>
2023-06-15 04:12:06 +0400

    add project

---
+++ java.js
@@ -0,0 +1,25 @@
+let month = "";
+
+switch (month) {
+    case "March":
+    case "April":
+    case "May":
+        console.log("Spring")
+        break;
+    case "June":
+    case "July":
+    case "August":
+        console.log("Summer")
+        break;
+    case "September":
+    case "October":
+    case "November":
+        console.log("Autumn")
+        break;
+    case "December":
+    case "January":
+    case "February":
+        console.log("Winter")
+    default:
+        console.log("This's not month")
+}
